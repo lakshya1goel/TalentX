edc867055717dae2cbe2186e64f4de57f10a5289
lakshya1goel <lakshya1234goel@gmail.com>
2025-08-31 19:00:21 +0530

    add ai client & config files
    Signed-off-by: lakshya1goel <lakshya1234goel@gmail.com>

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lakshya1goel/job-assistance/config"
+)
+
+func main() {
+	config.LoadEnv()
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Health Check!"})
+	})
+	router.Run(":8084")
+}
--- backend/internal/ai/client.go
+++ backend/internal/ai/client.go
@@ -1,1 +1,23 @@
 package ai
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/genai"
+)
+
+type AIClient struct {
+	Client *genai.Client
+}
+
+func NewAIClient(ctx context.Context, apiKey string) *AIClient {
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey: apiKey,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return &AIClient{Client: client}
+}
